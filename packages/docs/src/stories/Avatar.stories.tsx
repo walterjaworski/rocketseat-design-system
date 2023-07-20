56ada2b444e059c33fd8fdfb37990ecf54189be4
Walter Jaworski <walter.jaworski@gmail.com>
2023-07-20 14:30:58 -0300

    component control in storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/walterjaworski.png',
     alt: 'Walter Jaworski',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat nihil sunt consequatur ipsa saepe eveniet distinctio accusantium, porro labore voluptas sequi, facilis nostrum. Cumque itaque consequatur corrupti minima. Nesciunt, tenetur.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
